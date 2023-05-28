c33c75155832b7210499f5ec5efc686ae7965802
vekvoid <vektrm@gmail.com>
2023-05-28 13:53:37 +0000

    [Twitch Pinned Streamers] 1.0.3

--- Twitch Pinned Streamers/Twitch Pinned Streamers.user.js
+++ Twitch Pinned Streamers/Twitch Pinned Streamers.user.js
@@ -6,7 +6,7 @@
 // @supportURL  https://github.com/vekvoid/UserScripts/issues
 // @match        *://*.twitch.tv/*
 // @grant       none
-// @version     1.0.2
+// @version     1.0.3
 // ==/UserScript==
 
 const logLevels = {
