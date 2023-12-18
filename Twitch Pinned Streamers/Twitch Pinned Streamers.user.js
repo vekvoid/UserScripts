d9261a5d97456622c1dfc8342ef49ef2eccae3e6
vekvoid <vektrm@gmail.com>
2023-12-18 13:28:41 +0000

    [Twitch Pinned Streamers] 1.1.1

--- Twitch Pinned Streamers/Twitch Pinned Streamers.user.js
+++ Twitch Pinned Streamers/Twitch Pinned Streamers.user.js
@@ -7,7 +7,7 @@
 // @match        *://*.twitch.tv/*
 // @grant       none
 // @icon https://www.google.com/s2/favicons?domain=twitch.com
-// @version     1.1.0
+// @version     1.1.1
 // ==/UserScript==
 
 const logLevels = {
